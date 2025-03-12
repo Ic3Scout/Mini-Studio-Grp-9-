6d436e9102eb5c50e96b6c0bec92232f06ae354d
AlliV33 <alivock@gaming.tech>
2025-03-12 18:11:08 +0100

    mvmt fix

--- src/LightEngine/Player.cpp
+++ src/LightEngine/Player.cpp
@@ -6,7 +6,7 @@ void Player::MoveRight(float deltaTime)
 {
 	std::cout << "Player::MoveRight" << std::endl;
 
-	mSpeed += mParameters.mAcceleration* deltaTime;
+	mSpeed += mParameters.mAcceleration * deltaTime;
 	if (mSpeed > mParameters.mMaxSpeed)
 	{
 		mSpeed = mParameters.mMaxSpeed;
@@ -37,51 +37,48 @@ void Player::OnUpdate()
 {
 	float dt = GetDeltaTime();
 
-	//float x = sf::Joystick::getAxisPosition(0, sf::Joystick::Axis::X);
+	float x = sf::Joystick::getAxisPosition(0, sf::Joystick::Axis::X);
 
-	//if (sf::Joystick::isConnected(0))
-	//{
-	///*	std::cout << "JoyStick Connecte" << std::endl;*/
-	//}
-	//if (sf::Joystick::isButtonPressed(0, 1))
-	//{
-	//	std::cout << "X Pressed 1" << std::endl;
+	if (sf::Joystick::isConnected(0))
+	{
+		std::cout << "JoyStick Connecte" << std::endl;
+	}
+	if (sf::Joystick::isButtonPressed(0, 1))
+	{
+		std::cout << "X Pressed 1" << std::endl;
 
-	//	Jump();
-	//}
-	//if (sf::Joystick::isButtonPressed(0, 2))
-	//{
-	//	std::cout << "O Pressed 2" << std::endl;
-	//}
-	//if (sf::Joystick::isButtonPressed(0, 3))
-	//{
-	//	std::cout << "tri Pressed 3" << std::endl;
-	//}
-	//if (sf::Joystick::isButtonPressed(0, 4))
-	//{
-	//	std::cout << "car Pressed 4" << std::endl;
-	//}
-	//if (x > 25)
-	//{
-	//	mSpeed = 0.f;
-
-	//	MoveRight(dt);
-	//}
-	//else if (x < -25)
-	//{
-	//	mSpeed = 0.f;
-
-	//	MoveLeft(dt);
-	//}
-	//else
-	//{
-	//	mSpeed = 0.f;
-	//}
-	//std::cout << "axe X" << x << std::endl;
+		Jump();
+	}
+	if (sf::Joystick::isButtonPressed(0, 2))
+	{
+		std::cout << "O Pressed 2" << std::endl;
+	}
+	if (sf::Joystick::isButtonPressed(0, 3))
+	{
+		std::cout << "tri Pressed 3" << std::endl;
+	}
+	if (sf::Joystick::isButtonPressed(0, 4))
+	{
+		std::cout << "car Pressed 4" << std::endl;
+	}
+	if (x > 25)
+	{
+		MoveRight(dt);
+	}
+	else if (x < -25)
+	{
+		MoveLeft(dt);
+	}
+	else
+	{
+		mSpeed = 0.f;
+	}
+	std::cout << "axe X" << x << std::endl;
 
 
 
 
+	/*
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
 	{
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
@@ -106,6 +103,7 @@ void Player::OnUpdate()
 	{
 		Jump();
 	}
+	*/
 }
 
 void Player::OnCollision(Entity* other)
