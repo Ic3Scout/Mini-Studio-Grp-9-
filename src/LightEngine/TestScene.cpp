433c47b1f7e310e09b9e26f56127c9b17315f265
lgarrel <lgarrel@gaming.tech>
2025-03-13 11:54:33 +0100

    moteur

--- src/LightEngine/TestScene.cpp
+++ src/LightEngine/TestScene.cpp
@@ -17,33 +17,50 @@ void TestScene::OnInitialize()
 	pEntity1 = CreateEntity<PhysicalEntity>(50, sf::Color::Red);
 	pEntity1->SetPosition(width / 2, height / 2);
 	pEntity1->SetRigidBody(false);
+	pEntity1->SetHitbox(100, 100);
+	pEntity1->SetIsHitboxActive(false);
 
 	pEntity2 = CreateEntity<PhysicalEntity>(50, sf::Color::Red);
-	pEntity2->SetPosition(-500, height / 2);
+	pEntity2->SetPosition(width / 2 - 100, height / 2);
 	pEntity2->SetRigidBody(false);
+	pEntity2->SetHitbox(100, 100);
+	pEntity2->SetIsHitboxActive(false);
 
 	pEntitySelected = nullptr;
 }
 
 void TestScene::OnEvent(const sf::Event& event)
 {
-	if (event.type != sf::Event::EventType::MouseButtonPressed)
+	if (event.type != sf::Event::EventType::MouseButtonPressed && event.type != sf::Event::EventType::KeyPressed)
 		return;
+
+	sf::RenderWindow* win = GameManager::Get()->GetWindow();
+	sf::Vector2f mousePos = win->mapPixelToCoords(sf::Mouse::getPosition(*win));
 
 	if (event.mouseButton.button == sf::Mouse::Button::Right)
 	{
-		TrySetSelectedEntity(pEntity1, event.mouseButton.x, event.mouseButton.y);
-		TrySetSelectedEntity(pEntity2, event.mouseButton.x, event.mouseButton.y);
+		TrySetSelectedEntity(pEntity1, mousePos.x, mousePos.y);
+		TrySetSelectedEntity(pEntity2, mousePos.x, mousePos.y);
 	}
 
 	if (event.mouseButton.button == sf::Mouse::Button::Left)
 	{
 		if (pEntitySelected != nullptr)
 		{
-			sf::RenderWindow* win = GameManager::Get()->GetWindow();
-			sf::Vector2f mousePos = win->mapPixelToCoords(sf::Mouse::getPosition(*win));
 			pEntitySelected->GoToPosition(mousePos.x, mousePos.y, 200.f);
 		}
+	}
+
+	if (pEntitySelected != nullptr)
+	{
+		if (event.key.code == sf::Keyboard::H)
+		{
+			if (pEntitySelected->GetHitbox()->isActive)
+				pEntitySelected->SetIsHitboxActive(false);
+			else
+				pEntitySelected->SetIsHitboxActive();
+		}
+
 	}
 }
 
