d2f0528dfca930b0833e035d35137a5ac3bfb35c
AlliV33 <alivock@gaming.tech>
2025-03-16 23:17:48 +0100

    ca marche

--- src/LightEngine/TestScene.cpp
+++ src/LightEngine/TestScene.cpp
@@ -8,6 +8,21 @@
 
 #include <SFML/Graphics/RenderWindow.hpp>
 
+
+
+
+
+#include <fstream>
+#include <vector>
+#include <string>
+#include "SFML/Graphics.hpp"
+#include <filesystem>
+
+
+
+
+
+
 void TestScene::OnInitialize()
 {
 	int height = GetWindowHeight();
@@ -28,6 +43,53 @@ void TestScene::OnInitialize()
 
 	pEntity2->SetRigidBody(false);
 	pEntity2->SetGravity(false);
+
+
+
+
+
+
+	std::string filepath = "C:/Users/alfre/OneDrive/Documents/GitHub/Mini-Studio-Grp-9-/res/map.txt";
+	std::ifstream inputFile(filepath);
+
+	if (!std::filesystem::exists(filepath)) {
+		std::cerr << "Erreur : Le fichier n'existe pas à l'emplacement : " << filepath << std::endl;
+	}
+
+	if (!inputFile) {
+		std::cerr << "Erreur : Impossible d'ouvrir " << filepath << std::endl;
+	}
+
+	std::vector<PhysicalEntity*> platforms;
+	std::vector<std::string> map;
+
+	std::string line;
+	while (std::getline(inputFile, line)) {
+		map.push_back(line);
+	}
+
+	inputFile.close();
+
+	const int BLOCK_SIZE = 30;
+	int startX = width / 2;
+	int startY = height / 2;
+
+	for (size_t y = 0; y < map.size(); ++y) {
+		for (size_t x = 0; x < map[y].size(); ++x) {
+			if (map[y][x] == 'X') {
+				PhysicalEntity* block = CreateEntity<PhysicalEntity>(15, sf::Color::Red);
+				block->SetPosition(startX + x * BLOCK_SIZE, startY + y * BLOCK_SIZE);
+				block->SetRigidBody(false);
+				block->SetHitbox(BLOCK_SIZE, BLOCK_SIZE);
+				platforms.push_back(block);
+			}
+		}
+	}
+
+
+
+
+
 
 	pPlatforme1 = CreateEntity<PhysicalEntity>(sf::Vector2f(30, 30), sf::Color::Red);
 	pPlatforme1->SetPosition(width / 2, height / 2 + 300);
